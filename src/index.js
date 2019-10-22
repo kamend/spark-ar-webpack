652771d35f02c03a11705e9bd6b20ced301b9139
Kamen Dimitrov <kamend@gmail.com>
2019-10-22 16:32:46 +0300

    Made the effect export as a function

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const R = require('Reactive');
 const Shaders = require('Shaders');
 const Materials = require('Materials');
 
-import { filteredTexture as chuckCloseEffect } from './chuckclose';
+import { ChuckClose} from './chuckclose';
 import { filteredTexture as redEffect } from './redeffect';
 
 const mat = Materials.get("material0");
 const textureSlot = Shaders.DefaultMaterialTextures.DIFFUSE
 
-mat.setTexture(R.mul(chuckCloseEffect,redEffect), {textureSlotName: textureSlot});
+mat.setTexture(R.mul(ChuckClose(32,4,"material0", "cameraTexture0"),redEffect), {textureSlotName: textureSlot});
 
